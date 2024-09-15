8880d98d42edb5789a496259057a86a86287219f
DESKTOP-2KQT2IR\admin <eave824@gmail.com>
2024-09-16 00:28:01 +0700

    fix(myprofile): add cash navbar, add navbar filter add navbar hover animation

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,25 +1,35 @@
 
 
 interface Props {
-    values: any;
-    handleCategoryClick: any;
-    activeButton: any;
+    values: string[];
+    handleCategoryClick: (value: string) => void;
+    selectedBtn: string;
 }
 
 
-const Navbar = ({values, handleCategoryClick, activeButton }: Props) => {
-
-    return  (
-        <div className={`space-x-8 max-sm:space-x-4`}>
-            {values && values.map((value: any, index: any) => (
+const Navbar = ({values, handleCategoryClick, selectedBtn }: Props) => {
+    const filteredValues = values.filter((value) => value !== selectedBtn);
+    
+    return (
+        <div className="space-x-8 max-sm:space-x-4">
+            {filteredValues.map((value) => (
                 <button
-                    onClick={() => handleCategoryClick(index, value)}
-                    key={index}
-                    className={`uppercase ${index === activeButton ? 'text-black font-medium text-4xl max-sm:text-xl' : 'text-lg text-gray-500 max-sm:text-sm'}  `}
+                    onClick={() => handleCategoryClick(value)}
+                    key={value}
+                    className="transition hover:text-gray-700 uppercase text-lg text-gray-500 max-sm:text-sm"
                 >
                     {value}
                 </button>
             ))}
+            {selectedBtn && (
+                <button
+                    onClick={() => handleCategoryClick(selectedBtn)}
+                    className="uppercase text-black font-medium text-4xl max-sm:text-xl"
+                    key="active"
+                >
+                    {selectedBtn}
+                </button>
+            )}
         </div>
     )
 }
