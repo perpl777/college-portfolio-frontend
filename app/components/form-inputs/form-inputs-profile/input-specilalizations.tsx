b77a063e4e3a5ece247e12d0c2e3614ea969d1fc
TShuri <andr.xti@gmail.com>
2024-09-16 21:57:35 +0800

    Update input-specilalizations.tsx

--- app/components/form-inputs/form-inputs-profile/input-specilalizations.tsx
+++ app/components/form-inputs/form-inputs-profile/input-specilalizations.tsx
@@ -1,50 +1,78 @@
 'use client'
-import React, { useState, useEffect } from 'react';
-import { fetcher } from '@/lib/api';
+import React, { useState, useEffect } from 'react'
+import { fetcher } from '@/lib/api'
 
-
+import Box from '@mui/material/Box'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
 
 interface SpecializationsProps {
-    id: number;
-    attributes: {
-        name: string
-    }
+	id: number
+	attributes: {
+		name: string
+	}
 }
 
 interface Props {
-    selectedSpecialization: any
-    setSelectedSpecialization: any
+	selectedSpecialization: any
+	setSelectedSpecialization: any
 }
 
+export default function InputSpecializations({
+	selectedSpecialization,
+	setSelectedSpecialization,
+}: Props) {
+	const [specializations, setSpecializations] = useState<
+		SpecializationsProps[]
+	>([])
 
-export default function InputSpecializations({selectedSpecialization, setSelectedSpecialization}: Props) {
-    const [specializations, setSpecializations] = useState<SpecializationsProps[]>([]);
+	useEffect(() => {
+		const fetchData = async () => {
+			const specResponse = await fetcher(
+				`${process.env.NEXT_PUBLIC_STRAPI_URL}/specializations`
+			)
+			setSpecializations(specResponse.data)
+		}
+		fetchData()
+	}, [])
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const specResponse = await fetcher(`${process.env.NEXT_PUBLIC_STRAPI_URL}/specializations`);
-            setSpecializations(specResponse.data);
-        };
-        fetchData();
-    }, []); 
-    
+	const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		const selectedValue = e.target.value
+		setSelectedSpecialization(selectedValue)
+	}
 
-    const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const selectedValue = e.target.value;
-        setSelectedSpecialization(selectedValue);
-    };
+	const handleChange = (event: any) => {
+		setSelectedSpecialization(event.target.value)
+	}
 
-
-    return (
-        <div>
-            <select className="select select-bordered max-w-xs rounded-none w-full outline-none" value={selectedSpecialization} onChange={handleSelectChange}>
-                <option disabled selected>Специальность..</option>
+	return (
+		// <div>
+		//     <select className="select select-bordered max-w-xs rounded-none w-full outline-none" value={selectedSpecialization} onChange={handleSelectChange}>
+		//         <option disabled selected>Специальность..</option>
+		//         {specializations && specializations.map((value: SpecializationsProps) => (
+		//             <option key={value.id} value={value.id}>
+		//                 {value.attributes.name}
+		//             </option>
+		//         ))}
+		//     </select>
+		// </div>
+		<Box className="max-w-xs w-full rounded-none">
+			<FormControl fullWidth>
+				<InputLabel id='demo-simple-select-label'>Специальность</InputLabel>
+				<Select
+					labelId='demo-simple-select-label'
+					id='demo-simple-select'
+					value={selectedSpecialization}
+					label='Специальность'
+					onChange={handleChange}
+				>
                 {specializations && specializations.map((value: SpecializationsProps) => (
-                    <option key={value.id} value={value.id}>
-                        {value.attributes.name}
-                    </option>
-                ))}
-            </select>
-        </div>
-    );
+                    <MenuItem key={value.id} value={value.id}>{value.attributes.name}</MenuItem>
+		        ))}    
+				</Select>
+			</FormControl>
+		</Box>
+	)
 }
