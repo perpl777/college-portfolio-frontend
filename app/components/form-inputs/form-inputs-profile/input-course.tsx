a868f74b67202277441d53da4a8b30bb4d5ab444
TShuri <andr.xti@gmail.com>
2024-09-16 22:07:32 +0800

    Update input-course.tsx

--- app/components/form-inputs/form-inputs-profile/input-course.tsx
+++ app/components/form-inputs/form-inputs-profile/input-course.tsx
@@ -1,4 +1,8 @@
-
+import Box from '@mui/material/Box'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
 
 interface Props {
     selectedCourse: any
@@ -17,17 +21,36 @@ export default function InputCourse({selectedCourse, setSelectedCourse}: Props) 
         setSelectedCourse(selectedValue);
     };
 
+    const handleChange = (event: any) => {
+		setSelectedCourse(event.target.value)
+	}
 
     return (
-        <div>
-            <select required className="select select-bordered max-w-xs rounded-none w-full" value={selectedCourse} onChange={handleSelectChange}>
-                <option disabled selected>Курс..</option>
+        // <div>
+        //     <select required className="select select-bordered max-w-xs rounded-none w-full" value={selectedCourse} onChange={handleSelectChange}>
+        //         <option disabled selected>Курс..</option>
+        //         {course && course.map((value: any) => (
+        //             <option key={value} value={value}>
+        //                 {value}
+        //             </option>
+        //         ))}
+        //     </select>
+        // </div>
+        <Box className="max-w-36 w-full max-lg:max-w-xs">
+			<FormControl fullWidth>
+				<InputLabel id='demo-simple-select-label'>Курс</InputLabel>
+				<Select
+					labelId='demo-simple-select-label'
+					id='demo-simple-select'
+					value={selectedCourse}
+					label='Курс'
+					onChange={handleChange}
+				>
                 {course && course.map((value: any) => (
-                    <option key={value} value={value}>
-                        {value}
-                    </option>
-                ))}
-            </select>
-        </div>
+                    <MenuItem key={value} value={value}>{value}</MenuItem>
+		        ))}    
+				</Select>
+			</FormControl>
+		</Box>
     );
 }
