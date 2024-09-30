48b864c74f3a30bd6ab8303831d33c9992683f7d
DESKTOP-2KQT2IR\admin <eave824@gmail.com>
2024-10-01 05:25:07 +0700

    fix(tags): add horizontal scroll

--- app/components/tags.tsx
+++ app/components/tags.tsx
@@ -9,23 +9,25 @@ interface TagsProps {
 
 const Tags: React.FC<TagsProps> = ({ tags, filteredPost, handleTagFilter, selectedTags}) => {
     return (
-        <div className="flex gap-4 flex-wrap">
-            {tags && tags?.map((tag: any) => {
-                return (
-                    <label 
-                        key={tag} 
-                        className={`cursor-pointer px-3 border rounded-sm border-gray-300 py-1 text-sm text-gray-800 transition-colors ${selectedTags.includes(tag) ? 'bg-gray-200 border-gray-300/0' : ''}`} 
-                        style={{ userSelect: 'none' }}>
-                        <input
-                            type="checkbox"
-                            checked={filteredPost.includes(tag)}
-                            onChange={() => handleTagFilter(tag)}
-                            className="hidden"
-                        />
-                        # {tag}
-                    </label>
-                )
-            })}
+        <div className="overflow-x-auto overflow-y-hidden">
+            <div className="flex gap-4 flex-wrap max-h-[50vh] min-w-[800px] ">
+                {tags && tags?.map((tag: any) => {
+                    return (
+                        <label 
+                            key={tag} 
+                            className={`cursor-pointer px-3 border rounded-sm border-gray-300 py-1 text-sm text-gray-800 transition-colors ${selectedTags.includes(tag) ? 'bg-gray-200 border-gray-300/0' : ''}`} 
+                            style={{ userSelect: 'none' }}>
+                            <input
+                                type="checkbox"
+                                checked={filteredPost.includes(tag)}
+                                onChange={() => handleTagFilter(tag)}
+                                className="hidden"
+                            />
+                            # {tag}
+                        </label>
+                    )
+                })}
+            </div>
         </div>
     );
 };
